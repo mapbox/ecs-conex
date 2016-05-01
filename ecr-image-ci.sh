a93be7026a0e9b5e20a89a8495b4c7958774df52
Ryan Clark <ryan.clark.j@gmail.com>
2016-04-30 19:15:20 -0700

    more cleanup

--- ecr-image-ci.sh
+++ ecr-image-ci.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-set -e
+set -eu
 
-fastlog info "--> insure docker daemon is running"
+echo "insure docker daemon is running"
 service docker start > /dev/null 2>&1
 docker version > /dev/null 2>&1
 
@@ -14,6 +14,8 @@ owner=$(node -e "console.log(${Message}.repository.owner.name);")
 user=$(node -e "console.log(${Message}.pusher.name);")
 
 echo "processing commit ${after} by ${user} to ${ref} of ${owner}/${repo}"
+
+tmpdir="/mnt/data/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
 
 function before_image() {
   local region=$1
@@ -37,17 +39,24 @@ function insure_repo() {
     --repository-names ${repo} > /dev/null 2>&1 || create_repo ${region}
 }
 
-create_repo() {
+function create_repo() {
   local region=$1
   aws ecr create-repository --region ${region} --repository-name ${repo}
 }
 
-tmpdir="/mnt/data/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
-git clone https://github.com/${owner}/${repo} ${tmpdir}
+function cleanup() {
+  rm -rf ${tmpdir}
+}
+
+git clone https://${GithubAccessToken}@github.com/${owner}/${repo} ${tmpdir}
+trap "cleanup" EXIT
 cd ${tmpdir} && git checkout -q $after || exit 3
+if git describe --tags --exact-match 2> /dev/null; then
+  after="$(git describe --tags --exact-match)"
+fi
 
 echo "fetching previous image"
-insure_repo
+insure_repo us-east-1
 login us-east-1
 docker pull "$(before_image us-east-1)" > /dev/null 2>&1 || :
 
@@ -62,3 +71,5 @@ for region in "${regions[@]}"; do
   login ${region}
   docker push "$(after_image ${region})"
 done
+
+echo "completed successfully"
