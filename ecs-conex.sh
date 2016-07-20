e1a7550e9048eef19ee82b69d20cd3be2052a419
Emily DuBois <dubois@mapbox.com>
2016-07-20 19:42:26 -0400

    Adds ecs-conex unit tests (#32)
    * Refactors util functions
    
    * [wip] initial commit
    
    * Adds tests for credentials
    
    * A few unit tests from the top
    
    * Add region to aws ecr get-login call
    
    * Add a few more tests in test/utils.test.sh
    
    * Adds parseMessage test
    
    * Add github status and create repo tests
    
    * Move copy_func to its own function
    
    * Updates a few tests
    
    * Updates tests
    
    * Removed eval redefinition and uncommented tests
    
    * Adds assertion aliases
    
    * Updates cleanup and check_dockerfile functions
    
    * Refactors docker push functions, adds more tests
    
    * :elephant:
    
    * :no_good: circle test override
    
    * Adds exit code
    
    * More explicit env vars & testing file deps
    
    * Remove -O from credentials greps
    
    * move utils.sh into container

--- ecs-conex.sh
+++ ecs-conex.sh
@@ -5,97 +5,7 @@ set -o pipefail
 
 regions=(us-east-1 us-west-2 eu-west-1)
 tmpdir="$(mktemp -d /mnt/data/XXXXXX)"
-
-function before_image() {
-  local region=$1
-  echo ${AccountId}.dkr.ecr.${region}.amazonaws.com/${repo}:${before}
-}
-
-function after_image() {
-  local region=$1
-  local sha=${2:-${after}}
-  echo ${AccountId}.dkr.ecr.${region}.amazonaws.com/${repo}:${sha}
-}
-
-function login() {
-  local region=$1
-  eval "$(aws ecr get-login --region ${region})"
-}
-
-function ensure_repo() {
-  local region=$1
-  aws ecr describe-repositories \
-    --region ${region} \
-    --repository-names ${repo} > /dev/null 2>&1 || create_repo ${region}
-}
-
-function create_repo() {
-  local region=$1
-  aws ecr create-repository --region ${region} --repository-name ${repo} > /dev/null
-}
-
-function github_status() {
-  local status=$1
-  local description=$2
-
-  echo "sending ${status} status to github"
-
-  curl -s \
-    --request POST \
-    --header "Content-Type: application/json" \
-    --data "{\"state\":\"${status}\",\"description\":\"${description}\",\"context\":\"ecs-conex\"}" \
-    ${status_url} > /dev/null
-}
-
-function parse_message() {
-  ref=$(node -e "console.log(${Message}.ref);")
-  after=$(node -e "console.log(${Message}.after);")
-  before=$(node -e "console.log(${Message}.before);")
-  repo=$(node -e "console.log(${Message}.repository.name);")
-  owner=$(node -e "console.log(${Message}.repository.owner.name);")
-  user=$(node -e "console.log(${Message}.pusher.name);")
-  deleted=$(node -e "console.log(${Message}.deleted);")
-  status_url="https://api.github.com/repos/${owner}/${repo}/statuses/${after}?access_token=${GithubAccessToken}"
-}
-
-function credentials() {
-  role=$(curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/) || :
-  if [ -z "${role}" ]; then
-    return
-  fi
-
-  creds=$(curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/${role})
-  accessKeyId=$(node -e "console.log(${creds}.AccessKeyId)")
-  secretAccessKey=$(node -e "console.log(${creds}.SecretAccessKey)")
-  sessionToken=$(node -e "console.log(${creds}.SessionToken)")
-
-  args=""
-  if grep -O "ARG AWS_ACCESS_KEY_ID" ./Dockerfile > /dev/null 2>&1; then
-    [ -n "${accessKeyId}" ] && args="--build-arg AWS_ACCESS_KEY_ID=${accessKeyId} "
-  fi
-
-  if grep -O "ARG AWS_SECRET_ACCESS_KEY" ./Dockerfile > /dev/null 2>&1; then
-    [ -n "${secretAccessKey}" ] && args="--build-arg AWS_ACCESS_KEY_ID=${secretAccessKey} "
-  fi
-
-  if grep -O "ARG AWS_SESSION_TOKEN" ./Dockerfile > /dev/null 2>&1; then
-    [ -n "${sessionToken}" ] && args="--build-arg AWS_ACCESS_KEY_ID=${sessionToken}"
-  fi
-}
-
-function cleanup() {
-  exit_code=$?
-
-  parse_message
-
-  if [ "${exit_code}" == "0" ]; then
-    github_status "success" "ecs-conex successfully completed"
-  else
-    github_status "failure" "ecs-conex failed to build an image"
-  fi
-
-  rm -rf ${tmpdir}
-}
+source utils.sh
 
 function main() {
   echo "checking docker configuration"
@@ -112,16 +22,17 @@ function main() {
   parse_message
 
   echo "processing commit ${after} by ${user} to ${ref} of ${owner}/${repo}"
-  github_status "pending" "ecs-conex is building an image"
+
+  status="pending"
+  echo "sending ${status} status to github"
+  github_status "${status}" "ecs-conex is building an image"
   [ "${deleted}" == "true" ] && exit 0
 
   git clone https://${GithubAccessToken}@github.com/${owner}/${repo} ${tmpdir}
   cd ${tmpdir} && git checkout -q $after || exit 3
 
-  if [ ! -f ./Dockerfile ]; then
-    echo "no Dockerfile found"
-    exit 0
-  fi
+  echo "looking for dockerfile"
+  check_dockerfile ./Dockerfile
 
   echo "attempt to fetch previous image ${before} from ${StackRegion}"
   ensure_repo ${StackRegion}
@@ -129,29 +40,14 @@ function main() {
   docker pull "$(before_image ${StackRegion})" 2> /dev/null || :
 
   echo "gather local credentials and setup --build-arg"
-  credentials
+  credentials ./Dockerfile
 
   echo "building new image"
   docker build --quiet ${args} --tag ${repo} ${tmpdir}
-
-  for region in "${regions[@]}"; do
-    ensure_repo ${region}
-    login ${region}
-
-    echo "pushing ${after} to ${region}"
-    docker tag -f ${repo}:latest "$(after_image ${region})"
-    docker push "$(after_image ${region})"
-
-    if git describe --tags --exact-match 2> /dev/null; then
-      tag="$(git describe --tags --exact-match)"
-      echo "pushing ${tag} to ${region}"
-      docker tag -f ${repo}:latest "$(after_image ${region} ${tag})"
-      docker push "$(after_image ${region} ${tag})"
-    fi
-  done
+  docker_push
 
   echo "completed successfully"
 }
 
-trap "cleanup" EXIT
+trap "cleanup $?" EXIT
 main 2>&1 | FASTLOG_PREFIX='[${timestamp}] [ecs-conex] '[${MessageId}] fastlog info
---
+++ test/utils.test.sh
@@ -0,0 +1,362 @@
+#!/usr/bin/env bash
+
+set -eu
+source $(dirname $0)/../utils.sh
+source $(dirname $0)/lib/utils.sh
+FAILED=0
+PASSED=0
+
+# initialize test id counter
+testId=0
+
+# before_image() test
+tag_test "before_image()"
+export AccountId=1
+export repo=repo
+export before=1
+export after=2
+
+log=$(before_image us-east-1)
+expected="1.dkr.ecr.us-east-1.amazonaws.com/repo:1"
+assert "equal" "${log}" "${expected}"
+
+# after_image() 1 param test
+tag_test "after_image() with 1 param"
+export AccountId=1
+export repo=repo
+export before=1
+export after=2
+
+log=$(after_image us-east-1)
+expected="1.dkr.ecr.us-east-1.amazonaws.com/repo:2"
+assert "equal" "${log}" "${expected}"
+
+# after_image() 2 param test
+tag_test "after_image() with 2 params"
+export AccountId=1
+export repo=repo
+export before=1
+export after=2
+
+log=$(after_image us-east-1 v1.0.0)
+expected="1.dkr.ecr.us-east-1.amazonaws.com/repo:v1.0.0"
+assert "equal" "${log}" "${expected}"
+
+# login() test
+tag_test "login()"
+test_region=us-east-1
+
+function aws() {
+  if [ "${1}" != "ecr" ]; then
+    echo "First argument must be ecr"
+  elif [ "${2}" != "get-login" ]; then
+    echo "Second argument must be get-login"
+  elif [ "${4}" != "${test_region}" ]; then
+    echo "Fourth argument must be region"
+  else
+    echo "echo All good"
+  fi
+}
+
+log=$(login ${test_region})
+expected="All good"
+assert "equal" "${log}" "${expected}"
+
+# ensure_repo() setup
+copy_func create_repo old_create_repo
+test_region=us-east-1
+FAILURE_MESSAGE=""
+CALLED=0
+
+function aws() {
+  if [ "${1}" != "ecr" ]; then
+    FAILURE_MESSAGE="First argument must be ecr"
+  elif [ "${2}" != "describe-repositories" ]; then
+    FAILURE_MESSAGE="Second argument must be describe-repositories"
+  elif [ "${4}" != "${test_region}" ]; then
+    FAILURE_MESSAGE="Fourth argument must be region"
+  elif [ "${6}" == "exists" ]; then
+    return 0
+  elif [ "${6}" == "not_exists" ]; then
+    return 1
+  else
+    FAILURE_MESSAGE="${6} must be exists or not_exists"
+  fi
+}
+
+function create_repo() {
+  CALLED=1
+}
+
+# ensure_repo() exists test
+tag_test "ensure_repo() exists"
+repo=exists
+
+ensure_repo ${test_region}
+assert "equal" "${CALLED}" "0"
+assert "equal" "${FAILURE_MESSAGE}" "" "should not have any failures"
+
+# ensure_repo() doesn't exist test
+tag_test "ensure_repo() doesn't exist"
+repo="not_exists"
+
+ensure_repo ${test_region}
+assert "equal" "${CALLED}" "1"
+assert "equal" "${FAILURE_MESSAGE}" "" "should not have any failures"
+
+# ensure_repo() cleanup
+copy_func old_create_repo create_repo
+
+# create_repo() test
+tag_test "create_repo()"
+repo=repo
+test_region=us-east-1
+FAILURE_MESSAGE=""
+CALLED=0
+
+function aws() {
+  if [ "${1}" != "ecr" ]; then
+    FAILURE_MESSAGE="First argument must be ecr"
+  elif [ "$2" != "create-repository" ]; then
+    FAILURE_MESSAGE="Second argument must be create-repository"
+  elif [ "$4" != "${test_region}" ]; then
+    FAILURE_MESSAGE="Fourth argument must be region"
+  elif [ "$6" != "repo" ]; then
+    FAILURE_MESSAGE="Sixth argument must be repo"
+  else
+    CALLED=1
+  fi
+}
+
+create_repo ${test_region}
+assert "equal" "${FAILURE_MESSAGE}" "" "should not have any failures"
+assert "equal" "${CALLED}" "1"
+
+# github_status() test
+tag_test "github_status()"
+test_status="good"
+test_description="clear"
+status_url="https://api.github.com/repos/someone/stuff"
+FAILURE_MESSAGE=""
+CALLED=0
+
+function curl() {
+  if [ "${3}" != "POST" ]; then
+    FAILURE_MESSAGE="Must be a POST request"
+  elif [ "${7}" != "{\"state\":\"${test_status}\",\"description\":\"${test_description}\",\"context\":\"ecs-conex\"}" ]; then
+    FAILURE_MESSAGE="Must post correct body"
+  elif [ "${8}" != "${status_url}" ]; then
+    FAILURE_MESSAGE="Must post to the status url"
+  else
+    CALLED=1
+  fi
+}
+
+github_status ${test_status} ${test_description}
+assert "equal" "${FAILURE_MESSAGE}" "" "should not have any failures"
+assert "equal" "${CALLED}" "1"
+
+# check_dockerfile() test
+tag_test "check_dockerfile()"
+filepath="/fake/file/path"
+log=$(check_dockerfile ${filepath})
+assert "equal" "${log}" "no Dockerfile found"
+assert "equal" "$?" "0"
+
+filepath="ecs-conex.sh"
+log=$(check_dockerfile ${filepath})
+assert "equal" "${log}" ""
+
+# parse_message() test
+tag_test "parse_message()"
+Message=$(cat ./test/fixtures/message.test.json)
+GithubAccessToken=test
+parse_message
+assert "equal" "${status_url}" "https://api.github.com/repos/test/test/statuses/test?access_token=test"
+
+# credentials() setup
+tmpdocker=$(mktemp /tmp/dockerfile-XXXXXX)
+tmpcreds=$(cat ./test/fixtures/creds.test.json)
+
+function curl () {
+  nullRole=$(printenv | grep nullRole | sed 's/.*=//')
+  role=test_role
+
+  if [[ "${nullRole}" == "1" ]]; then
+    echo ""
+  elif [[ "${2}" != *"${role}"* ]]; then
+    echo ${role}
+  else
+    echo ${creds}
+  fi
+}
+
+function write_dockerfile() {
+  creds=$1
+  echo "ARG NPMToken" > ${tmpdocker}
+  echo "ARG AWS_ACCESS_KEY_ID" >> ${tmpdocker}
+  echo "ARG AWS_SECRET_ACCESS_KEY" >> ${tmpdocker}
+  echo "ARG AWS_SESSION_TOKEN" >> ${tmpdocker}
+}
+
+function clear_dockerfile() {
+  echo "" > ${tmpdocker}
+}
+
+# credentials() no npm token in env test
+tag_test "credentials() missing npm token in env"
+export NPMToken=""
+write_dockerfile "${tmpcreds}"
+credentials ${tmpdocker}
+assert "doesNotContain" "${args}" "NPMToken=${NPMToken}"
+
+# credentials() no npm token in dockerfile test
+tag_test "credentials() missing npm token in dockerfile"
+export NPMToken=test_NPMToken
+clear_dockerfile
+credentials ${tmpdocker}
+assert "doesNotContain" "${args}" "NPMToken=${NPMToken}"
+
+# credentials() no role test
+tag_test "credentials() missing role"
+export nullRole=1
+write_dockerfile "${tmpcreds}"
+credentials ${tmpdocker}
+assert "equal" "${args}" "--build-arg NPMToken=test_NPMToken"
+
+# credentials() role test
+tag_test "credentials() role"
+export nullRole=""
+write_dockerfile "${tmpcreds}"
+credentials ${tmpdocker}
+assert "contains" "${args}" "NPMToken=${NPMToken}"
+assert "contains" "${args}" "AWS_ACCESS_KEY_ID=$(node -e "console.log(${creds}.AccessKeyId)")"
+assert "contains" "${args}" "AWS_SECRET_ACCESS_KEY=$(node -e "console.log(${creds}.SecretAccessKey)")"
+assert "contains" "${args}" "AWS_SESSION_TOKEN=$(node -e "console.log(${creds}.SessionToken)")"
+
+# credentials() missing build arguments in dockerfile test
+tag_test "credentials() missing build arguments in dockerfile"
+clear_dockerfile
+credentials ${tmpdocker}
+assert "equal" "${args}" "" "should be empty"
+
+# credentials() missing build arguments in creds test
+tag_test "credentials() missing build arguments in creds"
+write_dockerfile "{}"
+credentials ${tmpdocker}
+assert "equal" "${args}" "--build-arg NPMToken=test_NPMToken"
+
+# exact_match() test
+region=us-east-1
+repo=test
+FAILURE=""
+
+function git () {
+  echo "test_tag"
+}
+
+function after_image {
+  if [ "${1}" != "us-east-1" ]; then
+    FAILURE="Region not passed into after_image"
+  elif [ "${2}" != "test_tag" ]; then
+    FAILURE="Tag not passed into after_image"
+  else
+    echo "some_after_image"
+  fi
+}
+
+function docker() {
+  if [ ${1} == "tag" ]; then
+    assert "equal" "${4}" "some_after_image"
+  elif [ ${1} == "push" ]; then
+    assert "equal" "${2}" "some_after_image"
+  else
+    FAILURE="should call docker tag or docker push"
+  fi
+}
+
+exact_match
+assert "equal" "${FAILURE}" ""
+
+# docker_push() test
+regions=(us-east-1)
+repo=test
+after=test
+FAILURE=""
+
+function ensure_repo() {
+  if [ "${1}" != "us-east-1" ]; then
+    FAILURE="Region not passed into ensure_repo"
+  fi
+}
+
+function login() {
+  if [ "${1}" != "us-east-1" ]; then
+    FAILURE="Region not passed into login"
+  fi
+}
+
+function after_image {
+  if [ "${1}" != "us-east-1" ]; then
+    FAILURE="Region not passed into after_image"
+  else
+    echo "some_after_image"
+  fi
+}
+
+function docker() {
+  if [ ${1} == "tag" ]; then
+    assert "equal" "${4}" "some_after_image"
+  elif [ ${1} == "push" ]; then
+    assert "equal" "${2}" "some_after_image"
+  else
+    FAILURE="should call docker tag or docker push"
+  fi
+}
+
+function git() {
+  exit 1
+}
+
+function exact_match() {
+  assert "equal" "${FAILURE}" ""
+}
+
+docker_push
+
+# cleanup()
+tag_test "cleanup()"
+tmpdir=$(mktemp -d /tmp/ecs-conex-test-XXXXXX)
+Message=$(cat ./test/fixtures/message.test.json)
+GithubAccessToken=test
+status=""
+message=""
+FAILURE=""
+
+function github_status() {
+  github_status=$1
+  github_message=$2
+}
+
+cleanup 1
+assert "equal" "${github_status}" "failure"
+assert "equal" "${github_message}" "ecs-conex failed to build an image"
+
+cleanup 0
+assert "equal" "${github_status}" "success"
+assert "equal" "${github_message}" "ecs-conex successfully completed"
+
+if [ -d ${tmpdir} ]; then
+  FAILURE="directory was not deleted"
+  rm -rf ${tmpdir}
+fi
+assert "equal" "${FAILURE}" "" "should not have any failures"
+
+# summary
+summarize
+
+if [[ ${FAILED} != 0 ]]; then
+  exit 1
+else
+  exit 0
+fi
