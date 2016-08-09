988c81a2d515ca73567c84e3001bbd3e6a6dd02c
Emily DuBois <emilymdubois@gmail.com>
2016-08-09 15:52:13 -0400

    Refactors to ignore non-GitSha tags and non-existent commits

--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -4,27 +4,68 @@ var queue = require('d3-queue').queue;
 var request = require('request');
 var _ = require('underscore');
 
-var user = process.argv[2];
-var repo = process.argv[3];
-var GithubAccessToken = process.env.GithubAccessToken;
-
 var ecr = new AWS.ECR();
 var params = {
-  repositoryName: repo
-};
+  user: process.argv[2],
+  repo: process.argv[3],
+  GithubAccessToken: process.env.GithubAccessToken,
+  maximumImages: 50
+}
 
-ecr.listImages(params, function (err, data) {
+ecr.listImages({ repositoryName: params.repo }, function (err, data) {
   if (err) console.log(err, err.stack);
-  var results = data.imageIds;
+  var result = data.imageIds;
+  var imageCount = validateECRSize(result, params);
+  result = isGitSha(result);
+  var deleteCount = numberToDelete(result, imageCount, params);
+  toDelete(result, deleteCount, params, function (err, res) {
+    if (err) throw new Error(err);
+    var imagesToDelete = res;
+    // Next steps:
+    //   - Write function to delete images
+    //   - Refactor this execution into its own function
+    //   - Tests
+  });
+})
 
-  var q = queue(10);
-  for (var i = 0; i < results.length; i++) {
-    var commit = results[i].imageTag;
-    if (commit === undefined) {
-      console.log('Image digest '+ results[i].imageDigest + ' did not have an image tag.');
+function validateECRSize(result, params) {
+  var imageCount = result.length;
+  if (imageCount < params.maximumImages) {
+    console.log('[ecs-conex cleanup]', params.repo, 'has', imageCount, 'images, which is less than', params.maximumImages + '. No clean-up required.')
+    process.exit(1)
+  } else {
+    return imageCount;
+  }
+}
+
+function isGitSha(array) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].imageTag !== undefined && array[i].imageTag.match(/^[a-z0-9]{40}$/)) {
+      array[i]['delete'] = '';
     } else {
+      console.log('[will not delete] Image tag "' + array[i].imageTag + '" did not resemble a GitSha.');
+      array[i]['delete'] = false;
+    }
+  }
+  return array;
+}
+
+function numberToDelete(array, imageCount, params) {
+  var deleteCount = imageCount - params.maximumImages;
+  for (var i = 0; i < array.length; i++) {
+    var match = _.isMatch(array[i], { delete: false });
+    if (match) deleteCount++
+  };
+  return deleteCount;
+}
+
+function toDelete(array, deleteCount, params, callback) {
+  var q = queue(10);
+  for (var i = 0; i < array.length; i++) {
+    var match = _.isMatch(array[i], { delete: '' });
+    if (match) {
       var options = {
-        url: 'https://api.github.com/repos/' + user + '/' + repo + '/commits/' + commit + '?access_token=' + GithubAccessToken,
+        url: 'https://api.github.com/repos/' + params.user + '/' + params.repo + '/commits/' + array[i].imageTag + '?access_token=' + params.GithubAccessToken,
         headers: { 'User-agent': 'request' }
       };
       q.defer(request, options);
@@ -32,12 +73,13 @@ ecr.listImages(params, function (err, data) {
   }
 
   q.awaitAll(function (error, response) {
-    if (error) throw new Error(error);
+    if (error) return callback(error);
     var results = [];
     for (var i = 0; i < response.length; i++) {
       if (response[i].statusCode !== 200) {
         var commit = response[i].request.uri.pathname.match(/\/([a-z0-9]*)$/)[1];
-        console.log('Image tag ' + commit + ' could not be retrieved from GitHub.');
+        deleteCount++;
+        console.log('[will not delete] Image tag ' + commit + ' could not be retrieved from GitHub.');
       } else {
         var result = JSON.parse(response[i].body);
         var object = {
@@ -47,7 +89,10 @@ ecr.listImages(params, function (err, data) {
         results.push(object);
       }
     };
-    var sorted = _.sortBy(results, function(o) { return o.date; });
-    console.log(sorted);
+    var sorted = _.sortBy(results, function(o) { return o.date * -1; });
+    var end = sorted.length - 1;
+    var start = end - deleteCount + 1;
+    var toDelete = sorted.splice(start, end);
+    return callback(null, toDelete);
   })
-})
+}
