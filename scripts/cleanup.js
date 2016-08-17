5523e6834eaa32c6af9544794c351421b308244b
Emily DuBois <emilymdubois@gmail.com>
2016-08-17 18:37:27 -0400

    Adds CLI script; refactors cleanup script; adds tests

--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -1,98 +1,145 @@
 var AWS = require('aws-sdk');
+var fs = require('fs');
 var moment = require('moment');
 var queue = require('d3-queue').queue;
 var request = require('request');
 var _ = require('underscore');
 
-var ecr = new AWS.ECR();
-var params = {
-  user: process.argv[2],
-  repo: process.argv[3],
-  GithubAccessToken: process.env.GithubAccessToken,
-  maximumImages: 50
+module.exports = {
+  'validateECRSize': validateECRSize,
+  'isGitSha': isGitSha,
+  'isWhitelisted': isWhitelisted,
+  'getTimeStamps': getTimeStamps,
+  'assignTimeStamps': assignTimeStamps,
+  'mergeByProperty': mergeByProperty,
+  'toDelete': toDelete,
+  'deleteImages': deleteImages
 }
 
-ecr.listImages({ repositoryName: params.repo }, function (err, data) {
-  if (err) console.log(err, err.stack);
-  var result = data.imageIds;
-  var imageCount = validateECRSize(result, params);
-  result = isGitSha(result);
-  var deleteCount = numberToDelete(result, imageCount, params);
-  toDelete(result, deleteCount, params, function (err, res) {
-    if (err) throw new Error(err);
-    var imagesToDelete = res;
-    // Next steps:
-    //   - Write function to delete images
-    //   - Refactor this execution into its own function
-    //   - Tests
-  });
-})
+if (!module.parent) {
+  var params = {
+    user: process.argv[2],
+    repo: process.argv[3],
+    whitelist: process.argv[4],
+    githubAccessToken: process.env.GithubAccessToken,
+    maximumImages: 50
+  };
+  params.whitelist = (params.whitelist) ? params.whitelist.split(',') : null;
+  run(params);
+}
 
-function validateECRSize(result, params) {
-  var imageCount = result.length;
-  if (imageCount < params.maximumImages) {
-    console.log('[ecs-conex cleanup]', params.repo, 'has', imageCount, 'images, which is less than', params.maximumImages + '. No clean-up required.')
-    process.exit(1)
-  } else {
-    return imageCount;
-  }
+function run(params) {
+  var ecr = new AWS.ECR();
+  listImages(ecr, params, function (err, res) {
+    if (err) throw new Error(err);
+    var result = res.imageIds;
+    validateECRSize(result, params);
+    result = isGitSha(result);
+    result = isWhitelisted(result, params);
+    getTimeStamps(result, params, function (err, res) {
+      if (err) throw new Error(err);
+      assignTimeStamps(result, res);
+      var imagesToDelete = toDelete(result, params);
+      deleteImages(imagesToDelete);
+    })
+  })
+}
+
+function listImages(ecr, params, callback) {
+  ecr.listImages({ repositoryName: params.repo }, function (err, data) {
+    if (err) return callback(err);
+    return callback(null, data);
+  })
+}
+
+function validateECRSize(array, params) {
+  var count = array.length;
+  if (count < params.maximumImages) {
+    throw new Error('[ecs-conex cleanup] '+ params.repo + ' has ' + count + ' images, which is less than ' + params.maximumImages + '. No clean-up required.');
+  };
 }
 
 function isGitSha(array) {
   for (var i = 0; i < array.length; i++) {
     if (array[i].imageTag !== undefined && array[i].imageTag.match(/^[a-z0-9]{40}$/)) {
-      array[i]['delete'] = '';
+      array[i]['ableToDelete'] = true;
     } else {
-      console.log('[will not delete] Image tag "' + array[i].imageTag + '" did not resemble a GitSha.');
-      array[i]['delete'] = false;
+      console.log('[will not delete] Image tag ' + array[i].imageTag + ' did not resemble a GitSha.');
+      array[i]['ableToDelete'] = false;
     }
   }
   return array;
 }
 
-function numberToDelete(array, imageCount, params) {
-  var deleteCount = imageCount - params.maximumImages;
+function isWhitelisted(array, params) {
   for (var i = 0; i < array.length; i++) {
-    var match = _.isMatch(array[i], { delete: false });
-    if (match) deleteCount++
-  };
-  return deleteCount;
+    if (params.whitelist !== null && params.whitelist.indexOf(array[i].imageTag) !== -1) {
+      console.log('[will not delete] Image tag ' + array[i].imageTag + ' is whitelisted.');
+      array[i]['ableToDelete'] = false;
+    }
+  }
+  return array;
 }
 
-function toDelete(array, deleteCount, params, callback) {
+function getTimeStamps(array, params, callback) {
   var q = queue(10);
   for (var i = 0; i < array.length; i++) {
-    var match = _.isMatch(array[i], { delete: '' });
+    var match = _.isMatch(array[i], { ableToDelete: true });
     if (match) {
       var options = {
-        url: 'https://api.github.com/repos/' + params.user + '/' + params.repo + '/commits/' + array[i].imageTag + '?access_token=' + params.GithubAccessToken,
+        url: 'https://api.github.com/repos/' + params.user + '/' + params.repo + '/commits/' + array[i].imageTag + '?access_token=' + params.githubAccessToken,
         headers: { 'User-agent': 'request' }
       };
       q.defer(request, options);
     }
   }
 
-  q.awaitAll(function (error, response) {
+  q.awaitAll(function(error, response) {
     if (error) return callback(error);
-    var results = [];
-    for (var i = 0; i < response.length; i++) {
-      if (response[i].statusCode !== 200) {
-        var commit = response[i].request.uri.pathname.match(/\/([a-z0-9]*)$/)[1];
-        deleteCount++;
-        console.log('[will not delete] Image tag ' + commit + ' could not be retrieved from GitHub.');
-      } else {
-        var result = JSON.parse(response[i].body);
-        var object = {
-          imageTag: result.sha,
-          date: moment(result.commit.author.date).unix()
-        };
-        results.push(object);
-      }
-    };
-    var sorted = _.sortBy(results, function(o) { return o.date * -1; });
-    var end = sorted.length - 1;
-    var start = end - deleteCount + 1;
-    var toDelete = sorted.splice(start, end);
-    return callback(null, toDelete);
+    return callback(null, response);
+  });
+}
+
+function assignTimeStamps(array, response) {
+  var dates = [];
+  for (var i = 0; i < response.length; i++) {
+    if (response[i].statusCode !== 200) {
+      var commit = response[i].request.uri.pathname.match(/\/([a-z0-9]*)$/)[1];
+      console.log('[will not delete] Image tag ' + commit + ' could not be retrieved from GitHub.');
+      dates.push({ imageTag: commit, ableToDelete: false });
+    } else {
+      var result = JSON.parse(response[i].body);
+      dates.push({ imageTag: result.sha, date: moment(result.commit.author.date).unix() });
+    }
+  };
+
+  mergeByProperty(array, dates, 'imageTag');
+  return array;
+}
+
+function mergeByProperty(arr1, arr2, prop) {
+  _.each(arr2, function(arr2object) {
+    var arr1object = _.find(arr1, function(arr1object) {
+      return arr1object[prop] === arr2object[prop];
+    });
+    arr1object ? _.extend(arr1object, arr2object) : console.log('[warning] Image tag ' + arr2object.imageTag + ' was queried for a commit date, but does not map to an ECR image.');
   })
+}
+
+function toDelete(array, params) {
+  var ableToDelete = [];
+  for (var i = 0; i < array.length; i++) {
+    var deletable = _.isMatch(array[i], { ableToDelete: true });
+    if (deletable) ableToDelete.push(array[i]);
+  }
+
+  var deleteCount = array.length - params.maximumImages;
+  var sorted = _.sortBy(ableToDelete, function(o) { return o.date * -1; });
+  var start = sorted.length - deleteCount;
+  var toDelete = sorted.splice(start, deleteCount);
+  return toDelete;
+}
+
+function deleteImages(array) {
+  console.log('[info] Deleting ' + array.length + ' images...');
 }
---
+++ test/cleanup.test.js
@@ -0,0 +1,107 @@
+var cleanup = require(__dirname + '/../scripts/cleanup.js');
+var test = require('tape');
+
+test('tests validateECRSize', function(assert) {
+  var validateECRSize = cleanup.validateECRSize;
+  var result = [
+    { imageTag: 'c5332a6c78cf23d86f28b8987a3ca78af46b7f48' },
+    { imageTag: '55ee14c984e9de11b5a1e9186b2dc6846bc10148' },
+    { imageTag: 'c9c0a271f985e51d4f6d7f9a545925822d4f6730' }
+  ];
+
+  assert.equal(validateECRSize(result, { maximumImages: 2 }), undefined);
+  assert.equal(validateECRSize(result, { maximumImages: 3 }), undefined)
+  assert.throws(function() { validateECRSize(result, { maximumImages: 4 }); }, /\[ecs-conex cleanup\] undefined has 3 images, which is less than 4. No clean-up required./);
+  assert.end();
+});
+
+test('tests isGitSha', function(assert) {
+  var isGitSha = cleanup.isGitSha;
+  var good = [{ imageTag: 'c5332a6c78cf23d86f28b8987a3ca78af46b7f48' }];
+  var bad = [
+    {  },
+    { imageTag: '' },
+    { imageTag: 'v0.1.0' },
+    { imageTag: 'c5332a6c78cf23d86f28b8987a3ca78af46b7f4' },
+    { imageTag: 'c5332a6c78cf23d86f28b8987a3ca78af46b7f488' },
+    { imageTag: 'C5332A6C78Cf23D86F28B8987A3CA78AF46B7F48' }
+  ];
+  bad = isGitSha(bad);
+
+  assert.plan(7);
+  assert.equal(isGitSha(good)[0].ableToDelete, true, 'should have a \"true\" ableToDelete value');
+  for (var i = 0; i < bad.length; i++) {
+    assert.equal(bad[i].ableToDelete, false, 'should have a \"false\" ableToDelete value');
+  };
+  assert.end();
+});
+
+test('tests isWhitelisted', function(assert) {
+  var isWhitelisted = cleanup.isWhitelisted;
+  var params = { whitelist: ['c5332a6c78cf23d86f28b8987a3ca78af46b7f48'] };
+
+  assert.deepEqual(isWhitelisted([{ imageTag: 'c5332a6c78cf23d86f28b8987a3ca78af46b7f48', ableToDelete: true }], params), [{ imageTag: 'c5332a6c78cf23d86f28b8987a3ca78af46b7f48', ableToDelete: false }]);
+  assert.deepEqual(isWhitelisted([{ imageTag: '', ableToDelete: true }], params), [{ imageTag: '', ableToDelete: true }]);
+  assert.end();
+});
+
+test('tests getTimestamps', function(assert) {
+  var getTimeStamps = cleanup.getTimeStamps;
+  var array = [{ imageTag: 'test', ableToDelete: true }];
+  var params = { user: 'test', repo: 'test', githubAccessToken: 'test' };
+
+  getTimeStamps(array, params, function(err, res) {
+    assert.ok(res[0].statusCode);
+    assert.ok(res[0].request.uri.pathname);
+    assert.ok(res[0].body);
+    assert.end();
+  });
+});
+
+test('tests assignTimeStamps', function(assert) {
+  var assignTimeStamps = cleanup.assignTimeStamps;
+
+  var array = [{ imageTag: 'c5332a6c78cf23d86f28b8987a3ca78af46b7f48', ableToDelete: true }];
+  var response200 = [{"statusCode": 200, "body": "{\"sha\":\"c5332a6c78cf23d86f28b8987a3ca78af46b7f48\",\"commit\":{\"author\":{\"date\":\"2016-07-20T18:27:53Z\"}}}", "request":{"uri":{"pathname": "/repos/user/repo/commits/c5332a6c78cf23d86f28b8987a3ca78af46b7f48" }}}];
+  assignTimeStamps(array, response200);
+  assert.deepEqual(array, [{ imageTag: 'c5332a6c78cf23d86f28b8987a3ca78af46b7f48', ableToDelete: true, date: 1469039273 }]);
+
+  var array = [{ imageTag: 'c5332a6c78cf23d86f28b8987a3ca78af46b7f48', ableToDelete: true }];
+  var response401 = [{"statusCode": 401, "body": "{\"sha\":\"c5332a6c78cf23d86f28b8987a3ca78af46b7f48\",\"commit\":{\"author\":{\"date\":\"2016-07-20T18:27:53Z\"}}}", "request":{"uri":{"pathname": "/repos/user/repo/commits/c5332a6c78cf23d86f28b8987a3ca78af46b7f48" }}}];
+  assignTimeStamps(array, response401);
+  assert.deepEqual(array, [{ imageTag: 'c5332a6c78cf23d86f28b8987a3ca78af46b7f48', ableToDelete: false }]);
+  assert.end();
+});
+
+test('tests mergeByProperty', function(assert) {
+  var mergeByProperty = cleanup.mergeByProperty;
+  var params = { deleteCount: 0 };
+
+  var arr1 = [{ imageTag: 'a', imageDigest: 'b', ableToDelete: true }];
+  var arr2 = [{ imageTag: 'a', date: '1469641800' }];
+  mergeByProperty(arr1, arr2, 'imageTag', params);
+  assert.deepEqual(arr1, [{ imageTag: 'a', imageDigest: 'b', date: '1469641800', ableToDelete: true }]);
+
+  var arr1 = [{ imageTag: 'a', imageDigest: 'b', ableToDelete: true }];
+  var arr3 = [{ imageTag: 'b', date: '1469641800' }];
+  mergeByProperty(arr1, arr3, 'imageTag', params);
+  assert.deepEqual(arr1, [{ imageTag: 'a', imageDigest: 'b', ableToDelete: true }]);
+  assert.end();
+});
+
+test('tests toDelete', function(assert) {
+  var toDelete = cleanup.toDelete;
+  var results = [
+    { imageTag: 'c5332a6c78cf23d86f28b8987a3ca78af46b7f48', ableToDelete: true,  date: 1471209422931 },
+    { imageTag: '55ee14c984e9de11b5a1e9186b2dc6846bc10148', ableToDelete: true,  date: 1471295822931 },
+    { imageTag: 'c9c0a271f985e51d4f6d7f9a545925822d4f6730', ableToDelete: true,  date: 1471123022931 },
+    { imageTag: '48a2a3e507ecb46e675d113cfa17e135e636b62b', ableToDelete: false, date: 1471235822931 },
+    { imageTag: 'a22b587d422aa18f25417838f6584144b5204e0c', ableToDelete: true,  date: 1471245822931 },
+  ];
+
+  assert.deepEqual(toDelete(results, { maximumImages: 1 }), [{ ableToDelete: true, date: 1471295822931, imageTag: '55ee14c984e9de11b5a1e9186b2dc6846bc10148' }, { ableToDelete: true, date: 1471245822931, imageTag: 'a22b587d422aa18f25417838f6584144b5204e0c' }, { ableToDelete: true, date: 1471209422931, imageTag: 'c5332a6c78cf23d86f28b8987a3ca78af46b7f48' }, { ableToDelete: true, date: 1471123022931, imageTag: 'c9c0a271f985e51d4f6d7f9a545925822d4f6730' }]);
+  assert.deepEqual(toDelete(results, { maximumImages: 2 }), [{ ableToDelete: true, date: 1471245822931, imageTag: 'a22b587d422aa18f25417838f6584144b5204e0c' }, { ableToDelete: true, date: 1471209422931, imageTag: 'c5332a6c78cf23d86f28b8987a3ca78af46b7f48' }, { ableToDelete: true, date: 1471123022931, imageTag: 'c9c0a271f985e51d4f6d7f9a545925822d4f6730' }]);
+  assert.deepEqual(toDelete(results, { maximumImages: 4 }), [{ ableToDelete: true, date: 1471123022931, imageTag: 'c9c0a271f985e51d4f6d7f9a545925822d4f6730' }]);
+  assert.deepEqual(toDelete(results, { maximumImages: 5 }), []);
+  assert.end();
+});
