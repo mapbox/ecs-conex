732d6ebb88b31fb8442f1b3d0b9ed15f65c66f62
dnomadb <damon@mapbox.com>
2016-10-27 15:54:10 -0700

    go back to quiet; make sure there is a token

--- ecs-conex.sh
+++ ecs-conex.sh
@@ -45,7 +45,8 @@ function main() {
   ecr_logins "${regions[@]}"
 
   echo "building new image"
-  docker build --no-cache ${args} --tag ${repo}:${after} ${tmpdir}
+  echo ${args} | grep -ioE "GithubAccessToken"
+  docker build --no-cache --quiet ${args} --tag ${repo}:${after} ${tmpdir}
   docker_push
 
   echo "completed successfully"
