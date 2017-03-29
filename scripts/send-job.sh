c118c0e9dd8b54e604c25344d2e4de5f2e7251fd
Emily DuBois <emilymdubois@gmail.com>
2017-03-29 13:27:16 -0400

    Adds GitHub validation

--- scripts/send-job.sh
+++ scripts/send-job.sh
@@ -7,20 +7,54 @@ Repo=$2
 AfterSha=$3
 BeforeSha=${4:-0000000000000000000000000000000000000000}
 Topic=$5
-Max=900
 
-# Fetches images for given repository, and sorts by image creation datetime.
-# Determine which of the oldest ECR images need to be deleted until remaning ECR
-# size is one less than ${Max}. Make an aws ecr batch-delete-image request for
-# these oldest messages.
+#################################
+# Make room in the ECR registry #
+#################################
+
+# Get a JSON of images from repository's ECR registry.
 response=$(aws ecr describe-images --repository-name ${Repo})
+
+# Isolate the imageDetails property.
 details=$(node -e "console.log(${response}.imageDetails)")
-validateGitSha=$(node -e "console.log(${details}.filter(function(e) { return /^[a-z0-9]{40}$/.test(e.imageTags[0]) }))")
-sorted=$(node -e "console.log(${validateGitSha}.sort(function(a, b) { return (a.imagePushedAt - b.imagePushedAt) }))")
+
+# Filter for imageTags that resemble GitShas.
+validated=$(node -e "console.log(${details}.filter(function(e) { return /^[a-z0-9]{40}$/.test(e.imageTags[0]) }))")
+
+# Sort images by creation datetime from earliest to latest.
+sorted=$(node -e "console.log(${validated}.sort(function(a, b) { return (a.imagePushedAt - b.imagePushedAt) }))")
+
+# Determine the number of images in the registry.
 length=$(node -e "console.log(${sorted}.length)")
-splice=$(node -e "console.log(${sorted}.splice(0, ${length} - ${Max} + 1))")
-images=$(node -e "console.log(${splice}.map(function(i) { return 'imageDigest=' + i.imageDigest; }).join(' '))")
-[ ! -z "$images" ] && aws ecr batch-delete-image --repository-name ${Repo} --image-ids ${images}
+
+# Join the imageTags in a bash-digestible format.
+joined=$(node -e "console.log(${sorted}.map(function(e) { return e.imageTags[0] }).join('\n'))")
+
+# Iterate through the sorted list of images. If the imageTag, or GitSha, does not
+# exist on GitHub, skip it. Do this until you have enough images to delete to bring
+# the registry size down to one less than the desired maximum.
+max=11
+toDelete=()
+
+numberToDelete=$(expr ${length} - ${max} + 1)
+[ "${numberToDelete}" -lt "0" ] && numberToDelete=0
+for tag in $joined; do
+  if [ "${#toDelete[@]}" -eq "${numberToDelete}" ]; then break; fi;
+  github=`curl -i https://api.github.com/repos/${Owner}/${Repo}/commits/${tag}?access_token=$GithubAccessToken | grep "Status: 200 OK"`
+  if [ ! -z "$github" ]; then
+    toDelete+=(imageDigest=$tag)
+  fi
+done
+
+# Join the images together, and make an AWS batch-delete-image request.
+if [ "${#toDelete[@]}" -ne 0 ]; then
+  images=$(echo "${toDelete[*]}")
+  aws ecr batch-delete-image --repository-name ${Repo} --image-ids ${images}
+fi
+
+#########################
+# Send job to SNS topic #
+#########################
 
 aws sns publish \
   --topic-arn ${Topic} \
