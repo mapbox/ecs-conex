11a9d8798c2bec56dbc5e0c5735d77a73d2fde9e
Emily DuBois <emilymdubois@gmail.com>
2017-03-29 14:02:19 -0400

    Better HTTP handling & some cleanup

--- scripts/send-job.sh
+++ scripts/send-job.sh
@@ -12,39 +12,41 @@ Topic=$5
 # Make room in the ECR registry #
 #################################
 
-# Get a JSON of images from repository's ECR registry.
+# Get a JSON of images from repository's ECR registry. Isolate the imageDetails
+# property. Filter for imageTags that resemble GitShas. Sort images by creation
+# datetime from earliest to latest. Join the imageTags in a bash-digestible format.
 response=$(aws ecr describe-images --repository-name ${Repo})
-
-# Isolate the imageDetails property.
 details=$(node -e "console.log(${response}.imageDetails)")
-
-# Filter for imageTags that resemble GitShas.
 validated=$(node -e "console.log(${details}.filter(function(e) { return /^[a-z0-9]{40}$/.test(e.imageTags[0]) }))")
-
-# Sort images by creation datetime from earliest to latest.
 sorted=$(node -e "console.log(${validated}.sort(function(a, b) { return (a.imagePushedAt - b.imagePushedAt) }))")
-
-# Determine the number of images in the registry.
-length=$(node -e "console.log(${sorted}.length)")
-
-# Join the imageTags in a bash-digestible format.
 joined=$(node -e "console.log(${sorted}.map(function(e) { return e.imageTags[0] }).join('\n'))")
 
 # Iterate through the sorted list of images. If the imageTag, or GitSha, does not
 # exist on GitHub, skip it. Do this until you have enough images to delete to bring
 # the registry size down to one less than the desired maximum.
-max=900
 toDelete=()
-
+couldNotDelete=()
+max=12
+length=$(node -e "console.log(${sorted}.length)")
 numberToDelete=$(expr ${length} - ${max} + 1)
+
 [ "${numberToDelete}" -lt "0" ] && numberToDelete=0
 for tag in $joined; do
-  if [ "${#toDelete[@]}" -eq "${numberToDelete}" ]; then break; fi;
-  github=`curl -i https://api.github.com/repos/${Owner}/${Repo}/commits/${tag}?access_token=$GithubAccessToken | grep "Status: 200 OK"`
-  if [ ! -z "$github" ]; then
-    toDelete+=(imageDigest=$tag)
-  fi
+  if [ "${#toDelete[@]}" -eq "${numberToDelete}" ]; then break; fi
+  httpCode=$(curl -s -o /dev/null -I -w "%{http_code}" https://api.github.com/repos/${Owner}/${Repo}/commits/${tag}?access_token=$GithubAccessToken)
+  if [ "$httpCode" -eq 200 ]; then toDelete+=(imageTag=$tag); else couldNotDelete+=(" * [${tag}] http code ${httpCode}\n"); fi
 done
+
+# If images need to be deleted but there aren't enough qualifying images, print
+# information about the GitShas and their HTTP codes. This could mean there is an
+# issue connecting to GitHub.
+if [ "${numberToDelete}" -ne 0 ] && [ "${#toDelete[@]}" -lt "${numberToDelete}" ]; then
+  echo -e "There was a problem finding enough images still on GitHub to delete:"
+  echo -e "  * Need to delete: ${numberToDelete} images"
+  echo -e "  * Able to delete: ${#toDelete[@]} images\n"
+  echo -e "The following GitShas returned non-200 status codes:"
+  echo -e " ${couldNotDelete[@]}"
+fi
 
 # Join the images together, and make an AWS batch-delete-image request.
 if [ "${#toDelete[@]}" -ne 0 ]; then
