b2809334f3b9bb756739cf70b7d59ba175a05fff
arunasank <aruna@mapbox.com>
2017-07-04 19:54:37 +0530

    validate using the github API

--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -7,15 +7,19 @@
 const AWS = require('aws-sdk');
 const region = process.argv[2];
 const repo = process.argv[3];
+const githubAccessToken = process.argv[4];
+const request = require('request');
+const queue = require('d3-queue').queue;
 
 if (!module.parent) {
   getImages(region, repo, (err, res) => {
     if (err) handleCb(err);
-    const imageIds = imagesToDelete(res);
-    if (!imageIds.length) handleCb(null, 'No images to delete');
-    deleteImages(region, repo, imageIds, (err, res) => {
-      if (err) handleCb(err);
-      if (res) handleCb(null, res);
+    imagesToDelete(res, (err, imageIds) => {
+      if (!imageIds.length) handleCb(null, 'No images to delete');
+      deleteImages(region, repo, imageIds, (err, res) => {
+        if (err) handleCb(err);
+        if (res) handleCb(null, res);
+      });
     });
   });
 }
@@ -45,13 +49,43 @@ function getImages(region, repo, callback) {
 }
 
 module.exports.imagesToDelete = imagesToDelete;
-function imagesToDelete(images) {
+function imagesToDelete(images, callback) {
+
   const max = 900;
   const validated = images.filter((e) => { return e.imageTags && /^[a-z0-9]{40}$/.test(e.imageTags[0]); });
   const sorted = validated.sort((a, b) => { return new Date(a.imagePushedAt) - new Date(b.imagePushedAt); });
-  const spliced = sorted.splice(0, images.length - max + 1);
-  const digests = spliced.map((e) => { return { imageDigest: e.imageDigest }; });
-  return digests;
+  githubCommitImages(sorted.splice(0, images.length - max + 1), (err, spliced) => {
+    const digests = spliced.map((e) => { return { imageDigest: e.imageDigest, imageTag: e.imageTag }; });
+    return callback(null, digests);
+  });
+}
+
+module.exports.githubCommitImages = githubCommitImages;
+function githubCommitImages(images, callback) {
+  let q = new queue(1);
+  let owner = 'mapbox';
+  let baseUrl = `https://api.github.com/repos/${owner}/${repo}/commits`;
+  images.forEach(image => {
+    let options = {
+      url: `${baseUrl}/${image.imageTags[0]}`,
+      headers: {
+        'User-Agent': 'mapbox',
+        Authorization: `token ${githubAccessToken}`
+      }
+    };
+    q.defer(getCommit, image, options);
+  });
+  q.awaitAll((err, validCommits) => {
+    return callback(null, validCommits);
+  });
+}
+
+function getCommit(image, options, callback) {
+  request.get(options, (err, response) => {
+    if (err || response.statusCode != 200)
+      return callback();
+    return callback(null, image);
+  });
 }
 
 module.exports.deleteImages = deleteImages;
