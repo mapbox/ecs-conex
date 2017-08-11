27c522c322a1116243834f279dafe075624d45a5
arunasank <aruna@mapbox.com>
2017-08-11 18:41:29 +0530

    tests and fixtures for retag

--- retag-existing.sh
+++ retag-existing.sh
@@ -1,6 +1,8 @@
+set -x
 SAVE_TAG="save"
 CLEANUP_TAG="cleanup"
-regions=(us-east-1 eu-west-1 us-west-2)
+regions=(us-east-1)
+# eu-west-1 us-west-2
 for region in "${regions[@]}"; do
     desc_repo_file="${region}-desc-repositories.json"
     repo_names_file="${region}-repositories.txt"
@@ -8,30 +10,40 @@ for region in "${regions[@]}"; do
     # know that there are < 160 images in all three regions, get 200 items at
     # a time
     aws ecr describe-repositories --max-items 200 --output json --region ${region} > ${desc_repo_file}
-    less ${desc_repo_file} | jq ' .repositories[] | .repositoryName' | sort | uniq > $repo_names_file
-    for repo in `cat $repo_names_file`; do
+    less ${desc_repo_file} | jq -r ' .repositories[] | .repositoryName' | sort | uniq > "${repo_names_file}"
+    for repo in `cat "${repo_names_file}"`; do
         # Clone the repo
-        git clone "git@github.com/mapbox/${repo}"
+        git clone "git@github.com:mapbox/${repo}.git"
         # Get in
         cd ${repo}
         # Get all the merge commits
-        git log --merges --format=format:%H > "${repo}-merge-commits"
-        git rev-list --all --remotes > "${repo}-all-commits"
+        git log --merges --format=format:%H > "$(dirname $0)/../${repo}-merge-commits"
+        git rev-list --all --remotes > "$(dirname $0)/../${repo}-all-commits"
         # Get all the commits that pertain to a tag OR 
         # pertain to a dereferenced tag
         # git show-ref --tags -d | cut -f 1 -d ' ' > "${repo}-tags"
-        git tag >> "${repo}-tags"
+        git tag >> "$(dirname $0)/../${repo}-tags"
         # Get each of these tags and check if an image exists for the same tag
         # Add a new tag "SAVE" to each of these images
-        aws ecr describe-images --repository-name ${repo} --output json | jq ' .imageDetails[].imageTags | select( length > 0) | join("\n")' > "${repo}-image-tags"
+        cd $(dirname $0)/../
+        aws ecr describe-images --repository-name ${repo} --output json | jq -r ' .imageDetails[].imageTags | select( length > 0) | join("\n")' > "${repo}-image-tags"
         for tag in `cat ${repo}-image-tags`; do
-            aws ecr batch-get-image --repository-name ${repo} --image-ids imageTag=${merge_commit} --query images[].imageManifest --output text > ${merge_commit}.manifest
+            aws ecr batch-get-image --repository-name ${repo} --image-ids imageTag=${tag} --query images[].imageManifest --output text > ${tag}.manifest
             if [[ -n `grep ${tag} "${repo}-merge-commits ${repo}-tags"` ]];
             then
-                aws ecr put-image --repository-name ${repo} --image-tag ${SAVE_TAG} --image-manifest ${merge_commit}.manifest
-            elif [[ -n `grep ${tag} "${repo}-all-commits"` ]]
-                aws ecr put-image --repository-name ${repo} --image-tag ${CLEANUP_TAG} --image-manifest ${merge_commit}.manifest
+                aws ecr put-image --repository-name ${repo} --image-tag ${SAVE_TAG} --image-manifest ${tag}.manifest
+            elif [[ -n `grep ${tag} "${repo}-all-commits"` ]];
+            then
+                aws ecr put-image --repository-name ${repo} --image-tag ${CLEANUP_TAG} --image-manifest ${tag}.manifest
+            elif [[ -z `grep ${tag} "${repo}-all-commits"` ]];
+            then
+                aws ecr put-image --repository-name ${repo} --image-tag ${SAVE_TAG} --image-manifest ${tag}.manifest
             fi
         done
+        rm "${repo}-merge-commits"
+        rm "${repo}-all-commits"
+        rm "${repo}-image-tags"
+        rm "${repo}-tags"
+        rm -rf "${repo}"
     done
 done
--- test/retag-existing.test.sh
+++ test/retag-existing.test.sh
@@ -1,5 +1,27 @@
+source $(dirname $0)/lib/utils.sh
+FAILED=0
+PASSED=0
+
+# initialize test id counter
+testId=0
 function aws() {
-	if [[ $2 == "describe-repositories"]]; then
-		`cat test/fixtures/desc-repo`
-	fi
+  if [[ $2 == "describe-repositories" ]]; then
+    cat "$(dirname $0)/fixtures/retag/desc_repo.json"
+  elif [[ $2 == "describe-images" ]]; then
+    cat "$(dirname $0)/fixtures/retag/desc_images.json"
+  elif [[ $2 == "batch-get-image" ]]; then
+    echo "manifest"
+  elif [[ $2 == "put-image" ]]; then
+    if [[ ${tag} == "d5e593e26d489bce9ba1407edf40e55bd9174ac1" ]]; then
+      assert "equal" "$6" "cleanup"
+    elif [[ ${tag} == "58d077f8357c9f507e106a09d4c73709a09623b6" ]]; then
+      assert "equal" "$6" "cleanup"
+    elif [[ ${tag} == "a5e3696f0d6450fae530c99a1b8598c71c11d41c" ]]; then
+      assert "equal" "$6" "save"
+    elif [[ ${tag} == "the-tiger" ]]; then
+      assert "equal" "$6" "save"
+    fi
+  fi
 }
+
+source $(dirname $0)/../retag-existing.sh
