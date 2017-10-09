3685a1f314d91c9fc899582baf8b5586bb1a08a6
Ryan Clark <ryan.clark.j@gmail.com>
2017-10-09 11:47:15 -0700

    test framework for image cleanup

--- test/cleanup.test.js
+++ test/cleanup.test.js
@@ -2,10 +2,14 @@
 
 /* eslint-disable no-console */
 
+const path = require('path');
+const os = require('os');
+const crypto = require('crypto');
 const child_process = require('child_process');
 const AWS = require('@mapbox/mock-aws-sdk-js');
 const sinon = require('sinon');
 const test = require('tape');
+const Queue = require('p-queue');
 const cleanup = require(`${__dirname}/../scripts/cleanup`);
 
 const region = 'us-east-1';
@@ -280,5 +284,246 @@ test('deleteimages, success', (assert) => {
 
     AWS.ECR.restore();
     assert.end();
+  });
+});
+
+const tests = [
+  { // < < <
+    start: { generic: 10, priority: 10, custom: 10 },
+    after: { generic: 10, priority: 10, custom: 10 }
+  },
+  { // < < =
+    start: { generic: 10, priority: 10, custom: 830 },
+    after: { generic: 10, priority: 10, custom: 830 }
+  },
+  { // < < >
+    start: { generic: 10, priority: 10, custom: 850 },
+    after: { generic: 0, priority: 10, custom: 850 }
+  },
+  { // < = <
+    start: { generic: 10, priority: 50, custom: 10 },
+    after: { generic: 10, priority: 50, custom: 10 }
+  },
+  { // < = =
+    start: { generic: 790, priority: 50, custom: 10 },
+    after: { generic: 790, priority: 50, custom: 10 }
+  },
+  { // < = >
+    start: { generic: 790, priority: 50, custom: 20 },
+    after: { generic: 780, priority: 50, custom: 20 }
+  },
+  { // < > <
+    start: { generic: 10, priority: 60, custom: 10 },
+    after: { generic: 10, priority: 60, custom: 10 }
+  },
+  { // < > =
+    start: { generic: 780, priority: 60, custom: 10 },
+    after: { generic: 780, priority: 60, custom: 10 }
+  },
+  { // < > >
+    start: { generic: 780, priority: 60, custom: 20 },
+    after: { generic: 770, priority: 60, custom: 20 }
+  },
+  { // = < =
+    start: { generic: 850, priority: 0, custom: 0 },
+    after: { generic: 850, priority: 0, custom: 0 }
+  },
+  { // = < >
+    start: { generic: 850, priority: 10, custom: 10 },
+    after: { generic: 830, priority: 10, custom: 10 }
+  },
+  { // = > >
+    start: { generic: 850, priority: 60, custom: 10 },
+    after: { generic: 780, priority: 60, custom: 10 }
+  },
+  { // > < >
+    start: { generic: 860, priority: 10, custom: 10 },
+    after: { generic: 830, priority: 10, custom: 10 }
+  },
+  { // > > >
+    start: { generic: 860, priority: 60, custom: 10 },
+    after: { generic: 780, priority: 60, custom: 10 }
+  },
+  { // full repository (1000 images), can get below max
+    start: { generic: 860, priority: 60, custom: 80 },
+    after: { generic: 710, priority: 60, custom: 80 }
+  },
+  { // full repository (1000 images), cannot get below max
+    start: { generic: 80, priority: 60, custom: 860 },
+    after: { generic: 0, priority: 50, custom: 860 }
+  }
+];
+
+const makeGitDir = () => new Promise((resolve, reject) => {
+  const dir = path.join(os.tmpdir(), crypto.randomBytes(4).toString('hex'));
+  child_process.exec(`mkdir -p ${dir}`, (err) => {
+    if (err) return reject(err);
+    child_process.exec('git init', { cwd: dir }, (err) => {
+      if (err) return reject(err);
+      resolve(dir);
+    });
+  });
+});
+
+const makeGenericCommit = (dir) => new Promise((resolve, reject) => {
+  child_process.exec('git commit -m "generic" --allow-empty', { cwd: dir }, (err) => {
+    if (err) return reject(err);
+    child_process.exec('git rev-parse HEAD', { cwd: dir }, (err, stdout) => {
+      if (err) return reject(err);
+      resolve(stdout.trim());
+    });
+  });
+});
+
+const makePriorityCommit = (dir) => new Promise((resolve, reject) => {
+  const tag = `tag.${crypto.randomBytes(4).toString('hex')}`;
+  child_process.exec('git commit -m "priority" --allow-empty', { cwd: dir }, (err) => {
+    if (err) return reject(err);
+    child_process.exec('git rev-parse HEAD', { cwd: dir }, (err, stdout) => {
+      if (err) return reject(err);
+      const sha = stdout.trim();
+      child_process.exec(`git tag ${tag}`, { cwd: dir }, (err) => {
+        if (err) return reject(err);
+        resolve([sha, tag]);
+      });
+    });
+  });
+});
+
+const images = (generic, priority, custom) => {
+  const imageDetails = [];
+
+  generic.forEach((sha) => imageDetails.push({
+    imageDigest: `sha256:${crypto.randomBytes(32).toString('hex')}`,
+    imageTags: [sha],
+    imagePushedAt: new Date().toISOString()
+  }));
+
+  priority.forEach((tags) => imageDetails.push({
+    imageDigest: `sha256:${crypto.randomBytes(32).toString('hex')}`,
+    imageTags: tags,
+    imagePushedAt: new Date().toISOString()
+  }));
+
+  custom.forEach((name) => imageDetails.push({
+    imageDigest: `sha256:${crypto.randomBytes(32).toString('hex')}`,
+    imageTags: [`custom.${name}`],
+    imagePushedAt: new Date().toISOString()
+  }));
+
+  return imageDetails;
+};
+
+const mockEcr = (images) => {
+  const backend = images.reduce((backend, image) => {
+    backend[image.imageDigest] = image;
+    return backend;
+  }, {});
+
+  const describeImages = AWS.stub('ECR', 'describeImages', function() {
+    images = JSON.parse(JSON.stringify(images));
+
+    this.request.eachPage = (callback) => {
+      const page = () => {
+        if (images.length) {
+          const data = { imageDetails: images.splice(0, 100) };
+          callback(null, data, page);
+        } else {
+          callback();
+        }
+      };
+
+      page();
+    };
+  });
+
+  const batchDeleteImage = AWS.stub('ECR', 'batchDeleteImage', function(params) {
+    params.imageIds.forEach((image) => {
+      delete backend[image.imageDigest];
+    });
+
+    this.request.promise.returns(Promise.resolve());
+  });
+
+  return{
+    describeImages,
+    batchDeleteImage,
+    remaining: () => Object.keys(backend).reduce(
+      (remaining, key) => {
+        const tags = backend[key].imageTags;
+
+        if (tags.length === 2) {
+          remaining.priority.push(tags);
+        } else if (/^custom\./.test(tags[0])) {
+          remaining.custom.push(tags);
+        } else {
+          remaining.generic.push(tags);
+        }
+        return remaining;
+      },
+      { generic: [], priority: [], custom: [] }
+    ),
+    restore: () => AWS.ECR.restore()
+  };
+};
+
+const setup = (state) => makeGitDir()
+  .then((dir) => {
+    const queue = new Queue({ concurrency: 1 });
+    const commitsToMake = [];
+
+    for (let i = 0; i < state.generic; i++) {
+      commitsToMake.push(
+        queue.add(() => makeGenericCommit(dir))
+      );
+    }
+
+    for (let j = 0; j < state.priority; j++) {
+      commitsToMake.push(
+        queue.add(() => makePriorityCommit(dir))
+      );
+    }
+
+    return Promise.all(commitsToMake);
+  })
+  .then((results) => {
+    // The collected SHAs, tags, and a set of custom strings to become named
+    // images in the fake ECR repository
+    const generic = results.slice(0, state.generic);
+    const priority = results.slice(state.generic);
+    const custom = new Array(state.custom).fill(0).map(
+      () => crypto.randomBytes(4).toString('hex')
+    );
+
+    return images(generic, priority, custom);
+  })
+  .then((images) => mockEcr(images));
+
+tests.forEach((state) => {
+  const name = `[ecr cleanup] generic: ${state.start.generic} | priority: ${state.start.priority} | custom: ${state.start.custom}`;
+  const msg = `---------- generic: ${state.after.generic} | priority: ${state.after.priority} | custom: ${state.after.custom}`;
+
+  test(name, (assert) => {
+    const expected = state.after;
+
+    setup(state.start)
+      .then((ecr) => {
+        const leftover = ecr.remaining();
+        assert.deepEqual(
+          {
+            generic: leftover.generic.length,
+            priority: leftover.priority.length,
+            custom: leftover.custom.length
+          },
+          expected,
+          msg
+        );
+        ecr.restore();
+      })
+      .catch((err) => assert.ifError(err, 'failed'))
+      .then(() => {
+        if (AWS.ECR.isSinonProxy) AWS.ECR.restore();
+        assert.end();
+      });
   });
 });
