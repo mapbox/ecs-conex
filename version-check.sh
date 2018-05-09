7c32162bc851c4c7e08079b3bbc974ac8096aae1
arunasank <aruna@mapbox.com>
2018-05-09 17:24:04 -0400

    echo

--- version-check.sh
+++ version-check.sh
@@ -3,6 +3,7 @@ function version-check() {
   major_server_docker_version=$(curl -s --unix-socket /var/run/docker.sock http://localhost/info | jq -r .ServerVersion | cut -d "." -f 1)
   echo "Host Docker version: ${major_server_docker_version}, Local Docker version: ${major_local_docker_version}"
   if [ $major_server_docker_version -ne $major_local_docker_version ]; then
+    echo "Docker versions don't match on the client and the host."
     aws sns publish \
     --topic-arn ${NotificationTopic} \
     --subject "Version mismatch between docker on ecs-conex and the host" \
